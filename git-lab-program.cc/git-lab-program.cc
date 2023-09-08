bcae71044d139a240f27c4f98dcd26d76009d633
Nick <ttvmultiface@gmail.com>
2023-09-08 18:07:31 -0400

    lab

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,17 @@
+//
+//  File: git-lab-program.cc
+//   Author: Nick Choi
+//   Date: 9/8/23
+//   Description: Prints "Hello Git!!"
+//
+
+#include <iostream> 
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argc[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+
+}//main
